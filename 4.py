a4bd87f8bd43ada03d70373ac78fb8446b035ad8
Renan <renanstrle@hotmail.com>
2017-12-03 17:07:23 -0200

    Item c done.

--- 4.py
+++ 4.py
@@ -4,7 +4,7 @@ from scipy import signal
 
 #a)
 Size = 12;
-T = np.arange(0, 4, 1/Size);
+T = np.arange(0, 1, 1/Size);
 X = signal.sawtooth(2*np.pi*T);
 
 Fig = plt.figure();
@@ -16,3 +16,22 @@ plt.grid(True);
 plt.show();
 
 #b)
+H = [0]*Size;
+H[int(0.25*Size)] = 1;
+Fig = plt.figure();
+plt.stem(T, H);
+plt.xlabel("N");
+plt.ylabel("δ(T − 0.25)");
+plt.title("b) Kronecker Delta");
+plt.grid(True);
+plt.show();
+
+#c)
+Y = np.convolve(X, H, "full");
+Fig = plt.figure();
+plt.stem(np.arange(0, 2-1/(Size-1), 1/Size), Y);
+plt.xlabel("N");
+plt.ylabel("X * H");
+plt.title("b) Convolution");
+plt.grid(True);
+plt.show();
