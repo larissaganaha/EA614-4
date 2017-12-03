8b14662aab19f80a290326ac19dedd8807e6b072
Renan <renanstrle@hotmail.com>
2017-12-03 18:17:05 -0200

    Item e done. Items a-d corrected.

--- 4.py
+++ 4.py
@@ -16,10 +16,10 @@ plt.grid(True);
 plt.show();
 
 #b)
-H = [0]*Size;
+H = [0]*8;
 H[int(0.25*Size)] = 1;
 Fig = plt.figure();
-plt.stem(T, H);
+plt.stem(T[0:8], H);
 plt.xlabel("N");
 plt.ylabel("δ(T − 0.25)");
 plt.title("b) Kronecker Delta");
@@ -29,7 +29,7 @@ plt.show();
 #c)
 Y = np.convolve(X, H, "full");
 Fig = plt.figure();
-plt.stem(np.arange(0, 2-1/(Size-1), 1/Size), Y);
+plt.stem(np.arange(0, 19/Size, 1/Size), Y);
 plt.xlabel("N");
 plt.ylabel("X * H");
 plt.title("c) Convolution");
@@ -37,19 +37,43 @@ plt.grid(True);
 plt.show();
 
 #d)
-FFTH = abs(np.fft.fft(H));
-FFTX = abs(np.fft.fft(X));
+FFTH = np.fft.fft(H);
+FFTX = np.fft.fft(X);
 K = Size*T/2-Size/4;
 Fig, F = plt.subplots(2, sharex=True, sharey=True);
-F[0].stem(K, np.concatenate((FFTX[int(Size/2):Size], FFTX[0:int(Size/2)])));
+F[0].stem(K, abs(np.concatenate((FFTX[int(Size/2):Size], FFTX[0:int(Size/2)]))));
 F[0].set_title("X Spectrum");
 F[0].grid(True);
-F[1].stem(K, np.concatenate((FFTH[int(Size/2):Size], FFTH[0:int(Size/2)])));
-F[1].set_title("Y Spectrum");
+F[1].stem(K[2:10], abs(np.concatenate((FFTH[4:8], FFTH[0:4]))));
+F[1].set_title("H Spectrum");
 F[1].grid(True);
 plt.xlabel("K[Hz]");
-plt.ylabel("|FFTH|");
+plt.ylabel("|FFT|");
+plt.show();
+
+Fig, F = plt.subplots(2, sharex=True, sharey=True);
+F[0].stem(K, np.angle(np.concatenate((FFTX[int(Size/2):Size], FFTX[0:int(Size/2)])), deg=True));
+F[0].set_title("X Phase Plot");
+F[0].grid(True);
+F[1].stem(K[2:10], np.angle(np.concatenate((FFTH[4:8], FFTH[0:4])), deg=True));
+F[1].set_title("H Phase Plot");
+F[1].grid(True);
+plt.xlabel("K[Hz]");
+plt.ylabel("Phase[°]");
+plt.show();
 
 #e)
-
+SpectrumY = [0]*Size;
+for I in range(0, 8): SpectrumY[I] = FFTX[I] * FFTH[I];
+Fig, F = plt.subplots(2, sharex=True);
+F[0].stem(K, abs(np.concatenate((SpectrumY[int(Size/2):Size], SpectrumY[0:int(Size/2)]))));
+F[0].set_title("|FFTY|");
+plt.grid(True);
+F[1].stem(K, np.angle(SpectrumY, deg=True));
+F[1].set_title("Y Phase[°]");\
+plt.xlabel("K[Hz]");
+plt.grid(True);
 plt.show();
+
+#f)
+
