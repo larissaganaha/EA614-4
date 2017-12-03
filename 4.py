793ade34d0139f16dea78c06442acc4fd93185f0
Renan <renanstrle@hotmail.com>
2017-12-03 17:35:26 -0200

    Item d done.

--- 4.py
+++ 4.py
@@ -32,6 +32,24 @@ Fig = plt.figure();
 plt.stem(np.arange(0, 2-1/(Size-1), 1/Size), Y);
 plt.xlabel("N");
 plt.ylabel("X * H");
-plt.title("b) Convolution");
+plt.title("c) Convolution");
 plt.grid(True);
+plt.show();
+
+#d)
+FFTH = abs(np.fft.fft(H));
+FFTX = abs(np.fft.fft(X));
+K = Size*T/2-Size/4;
+Fig, F = plt.subplots(2, sharex=True, sharey=True);
+F[0].stem(K, np.concatenate((FFTX[int(Size/2):Size], FFTX[0:int(Size/2)])));
+F[0].set_title("X Spectrum");
+F[0].grid(True);
+F[1].stem(K, np.concatenate((FFTH[int(Size/2):Size], FFTH[0:int(Size/2)])));
+F[1].set_title("Y Spectrum");
+F[1].grid(True);
+plt.xlabel("K[Hz]");
+plt.ylabel("|FFTH|");
+
+#e)
+
 plt.show();
